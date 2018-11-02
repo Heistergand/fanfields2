ab327952bafee809316424336c7da25877456d8b
Heistergand <16416532+Heistergand@users.noreply.github.com>
2018-11-02 20:33:23 +0100

    Create iitc_plugin_fanfields2.meta.js

---
+++ iitc_plugin_fanfields2.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @id              fanfields@heistergand
+// @name            IITC plugin: Fan Fields 2
+// @author          Heistergand
+// @category        Layer
+// @version         2.1.3
+// @description     Calculate how to link the portals to create the largest tidy set of nested fields. Enable from the layer chooser.
+// @include         https://*.ingress.com/intel*
+// @include         http://*.ingress.com/intel*
+// @include         https://*.ingress.com/mission/*
+// @include         http://*.ingress.com/mission/*
+// @match           https://*.ingress.com/intel*
+// @match           http://*.ingress.com/intel*
+// @match           https://*.ingress.com/mission/*
+// @match           http://*.ingress.com/mission/*
+// @grant           none
+
+// @downloadURL https://userscripts.enl.one/script/download/undefined/9eb34f67b379f2f27a3e5264c4332d69.user.js
+
+// @updateURL https://userscripts.enl.one/script/download/undefined/9eb34f67b379f2f27a3e5264c4332d69.meta.js
+// ==/UserScript==
