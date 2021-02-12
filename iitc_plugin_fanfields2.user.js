e9bc81b3fbdc2a3f19d7dfb7bc120fec0e5d3418
Heistergand <info@heistergand.com>
2021-02-12 19:58:51 +0100

    Update iitc_plugin_fanfields2.user.js
    getting changes to my fork

--- iitc_plugin_fanfields2.user.js
+++ iitc_plugin_fanfields2.user.js
@@ -8,8 +8,8 @@
 // @include         https://intel.ingress.com/*
 // @match           https://intel.ingress.com/*
 // @grant           none
-// @downloadURL https://github.com/zysfryar/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
-// @updateURL https://github.com/zysfryar/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
+// @downloadURL https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
+// @updateURL https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
 // ==/UserScript==
 
 /*
