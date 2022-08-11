eaa4205a329b0926dca5f91e29fefc7810d0bec8
Heistergand <info@heistergand.com>
2022-08-11 19:36:32 +0200

    Update iitc_plugin_fanfields2.user.js
    removed deprecated @include tag

--- iitc_plugin_fanfields2.user.js
+++ iitc_plugin_fanfields2.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
-// @id              fanfields@heistergand
 // @name            IITC plugin: Fan Fields 2
+// @id              fanfields@heistergand
 // @author          Heistergand
 // @category        Layer
-// @version         2.1.9.1
+// @version         2.1.9.2
 // @description     Calculate how to link the portals to create the largest tidy set of nested fields. Enable from the layer chooser.
-// @include         https://intel.ingress.com/*
 // @match           https://intel.ingress.com/*
 // @grant           none
 // @downloadURL https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
