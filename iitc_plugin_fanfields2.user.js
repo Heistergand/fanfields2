41da5bd8baaa0b215995a983953eb741aca31f7a
Heistergand <info@heistergand.com>
2022-09-04 23:19:27 +0200

    Added favicon.ico to script header.

--- iitc_plugin_fanfields2.user.js
+++ iitc_plugin_fanfields2.user.js
@@ -3,21 +3,24 @@
 // @id              fanfields@heistergand
 // @author          Heistergand
 // @category        Layer
-// @version         2.2.1
+// @version         2.2.2
 // @description     Calculate how to link the portals to create the largest tidy set of nested fields. Enable from the layer chooser.
 // @match           https://intel.ingress.com/*
 // @include         https://intel.ingress.com/*
 // @grant           none
 // @downloadURL     https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
 // @updateURL       https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
-// @icon            https://www.google.com/s2/favicons?sz=64&domain=intel.ingress.com
+// @icon            https://raw.githubusercontent.com/Heistergand/fanfields2/master/favicon.ico
 // @supportURL      https://github.com/Heistergand/fanfields2/issues
 // @namespace       https://github.com/Heistergand/fanfields2
 // ==/UserScript==
-
 /*
 
 Version History:
+
+2.2.2 (Heistergand)
+NEW: Added favicon.ico to script header.
+
 2.2.1 (Heistergand via Jormund)
 FIX: Merged from Jormund fork (2.1.7): Fixed L.LatLng extension
 
