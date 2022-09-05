a5c7a59c66cd93a7e0a4f21e4c7b55c1290c1d25
Heistergand <info@heistergand.com>
2022-09-05 19:26:04 +0200

    Update Header to Version 2.2.3

--- iitc_plugin_fanfields2.meta.js
+++ iitc_plugin_fanfields2.meta.js
@@ -1,9 +1,10 @@
+
 // ==UserScript==
 // @name            IITC plugin: Fan Fields 2
 // @id              fanfields@heistergand
 // @author          Heistergand
 // @category        Layer
-// @version         2.2.2
+// @version         2.2.3
 // @description     Calculate how to link the portals to create the largest tidy set of nested fields. Enable from the layer chooser.
 // @match           https://intel.ingress.com/*
 // @include         https://intel.ingress.com/*
