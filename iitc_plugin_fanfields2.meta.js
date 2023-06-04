1ab86c1c79604d3311fe0b7796658394e100ebdf
Heistergand <info@heistergand.com>
2023-06-04 03:56:12 +0200

    new icons

--- iitc_plugin_fanfields2.meta.js
+++ iitc_plugin_fanfields2.meta.js
@@ -8,9 +8,10 @@
 // @match           https://intel.ingress.com/*
 // @include         https://intel.ingress.com/*
 // @grant           none
-// @downloadURL     https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
-// @updateURL       https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
-// @icon            https://raw.githubusercontent.com/Heistergand/fanfields2/master/favicon.ico
+// @downloadURL     https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.user.js
+// @updateURL       https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.meta.js
+// @icon            https://github.com/Heistergand/fanfields2/blob/bf2af2fe3cb49e0777226c07777e82aad4368ce3/fanfields2-16.png
+// @icon64          https://github.com/Heistergand/fanfields2/blob/bf2af2fe3cb49e0777226c07777e82aad4368ce3/fanfields2-64.png
 // @supportURL      https://github.com/Heistergand/fanfields2/issues
 // @namespace       https://github.com/Heistergand/fanfields2
 // ==/UserScript==
