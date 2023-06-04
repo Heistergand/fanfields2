9f25a762b78626e6a1066326d302b99a44bae7f6
Heistergand <info@heistergand.com>
2023-06-04 22:39:06 +0200

    Update iitc_plugin_fanfields2.user.js

--- iitc_plugin_fanfields2.user.js
+++ iitc_plugin_fanfields2.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name            IITC plugin: Fan Fields 2
+// @name            IITC plugin: Fan Fields 2 (beta)
 // @id              fanfields@heistergand
 // @author          Heistergand
 // @category        Layer
