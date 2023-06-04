7e347edc5625fc7c435c2a261f485783193c7ec0
Heistergand <info@heistergand.com>
2023-06-04 04:04:40 +0200

    Update iitc_plugin_fanfields2.user.js

--- iitc_plugin_fanfields2.user.js
+++ iitc_plugin_fanfields2.user.js
@@ -8,15 +8,14 @@
 // @match           https://intel.ingress.com/*
 // @include         https://intel.ingress.com/*
 // @grant           none
-// @downloadURL     https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.user.js
 // @updateURL       https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.meta.js
+// @downloadURL     https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.user.js
 // @icon            https://github.com/Heistergand/fanfields2/blob/bf2af2fe3cb49e0777226c07777e82aad4368ce3/fanfields2-16.png
 // @icon64          https://github.com/Heistergand/fanfields2/blob/bf2af2fe3cb49e0777226c07777e82aad4368ce3/fanfields2-64.png
 // @supportURL      https://github.com/Heistergand/fanfields2/issues
 // @namespace       https://github.com/Heistergand/fanfields2
 // ==/UserScript==
 /*
-
 
 Version History:
 
