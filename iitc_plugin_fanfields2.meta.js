8d15d2d671b4286b25a3c02fff1e90d6e6c403e9
Heistergand <info@heistergand.com>
2023-06-11 23:41:28 +0200

    Update iitc_plugin_fanfields2.meta.js

--- iitc_plugin_fanfields2.meta.js
+++ iitc_plugin_fanfields2.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name            IITC plugin: Fan Fields 2 (beta)
+// @name            IITC plugin: Fan Fields 2
 // @id              fanfields@heistergand
 // @author          Heistergand
 // @category        Layer
@@ -8,10 +8,10 @@
 // @match           https://intel.ingress.com/*
 // @include         https://intel.ingress.com/*
 // @grant           none
-// @downloadURL     https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.user.js
-// @updateURL       https://github.com/Heistergand/fanfields2/raw/beta/iitc_plugin_fanfields2.meta.js
-// @icon            https://github.com/Heistergand/fanfields2/raw/beta/fanfields2-16.png
-// @icon64          https://github.com/Heistergand/fanfields2/raw/beta/fanfields2-64.png
+// @downloadURL     https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.user.js
+// @updateURL       https://github.com/Heistergand/fanfields2/raw/master/iitc_plugin_fanfields2.meta.js
+// @icon            https://github.com/Heistergand/fanfields2/raw/master/fanfields2-16.png
+// @icon64          https://github.com/Heistergand/fanfields2/raw/master/fanfields2-64.png
 // @supportURL      https://github.com/Heistergand/fanfields2/issues
 // @namespace       https://github.com/Heistergand/fanfields2
 // ==/UserScript==
